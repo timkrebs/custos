c7080232008814b35958e6b1213a16d229590c34
Tim Krebs <timkrebs9@gmail.com>
2026-04-11 15:42:40 +0200

    refactor(test): remove unused TestParseHCLConfig function and imports

--- pkg/parser/hcl_test.go
+++ pkg/parser/hcl_test.go
@@ -1,12 +1,1 @@
 package parser
-
-import (
-	"fmt"
-	"testing"
-)
-
-func TestParseHCLConfig(t *testing.T) {
-	config := ParseHCLConfig("testdata/config.hcl")
-	fmt.Printf("Parsed HCL Config: %+v\n", config)
-
-}
